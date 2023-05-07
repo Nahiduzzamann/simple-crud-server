4f878f9683c9824a765d8525e3b755b13de970f1
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-05-07 11:44:53 +0100

    delete done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //midleware
 app.use(cors());
@@ -43,6 +43,12 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/users/:id', async(req,res)=>{
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
