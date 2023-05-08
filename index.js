e66a2568a477d8c473146f4f3c558a354848f5e6
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-05-08 07:37:52 +0100

    update done

--- index.js
+++ index.js
@@ -47,7 +47,21 @@ async function run() {
             const result = await userCollection.insertOne(user)
             res.send(result)
         })
+        app.put('/users/:id', async(req, res)=>{
+            const id = req.params.id
+           const user=req.body;
+           const filter = {_id: new ObjectId(id)}
+           const option = {upsert:true}
+           const updatedUser = {
+            $set: {
+                name: user.name,
+                email: user.email
+            }
+           }
+           const result = await userCollection.updateOne(filter,updatedUser,option);
 
+            res.send(result)
+        })
         app.delete('/users/:id', async(req,res)=>{
             const id = req.params.id
             const query = {_id: new ObjectId(id)}
